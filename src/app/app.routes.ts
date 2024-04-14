193872dfa6f2e81c2a3746897ae522f99e40b672
Paulo Cesar Charamba de Souza <paulo.charamba@gmail.com>
2024-04-14 17:47:22 -0300

    Página inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListaComponent } from './pessoas/lista/lista.component';
+import { HomeComponent } from './home/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'pessoas', component: ListaComponent },
 ];
