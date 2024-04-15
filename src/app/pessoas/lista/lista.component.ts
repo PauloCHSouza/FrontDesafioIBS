bc9dcb4f698d889b6051bcd6421efe0c366b2e1f
Paulo Cesar Charamba de Souza <paulo.charamba@gmail.com>
2024-04-15 00:00:03 -0300

    Paginação e filtro

--- src/app/pessoas/lista/lista.component.ts
+++ src/app/pessoas/lista/lista.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { PessoasService } from '../../services/pessoas.service';
-import { CommonModule } from '@angular/common';
 import { Pessoa } from '../../Interfaces/pessoas';
 import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AddPessoaModalComponent } from '../add-pessoa-modal/add-pessoa-modal.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, NgxPaginationModule, FormsModule ],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
+  p: number = 1;
+  total: number = 0;
   pessoas: Pessoa[] = [];
+
+  filtro = {
+    nome: '',
+    sexo: '',
+    estadoCivil: '',
+    data: ''
+  };
+
+  pessoasFiltradas: Pessoa[] = [];
 
   constructor(
     private pessoasService: PessoasService,
@@ -27,6 +40,9 @@ export class ListaComponent {
   loadPessoas(): void {
     this.pessoasService.getPessoas().subscribe(data => {
         this.pessoas = data;
+        this.pessoasFiltradas = data;
+        // Aplicar filtros quando dados são carregados
+        this.aplicarFiltros();
       },
       (error) => {
         console.error('Erro ao carregar pessoas:', error);
@@ -97,5 +113,32 @@ export class ListaComponent {
         });
       }
     });
+  }
+
+  pageChangeEvent(event: number){
+    this.p = event;
+    // Recarregar a lista quando a página muda
+    this.loadPessoas();
+  }
+
+  aplicarFiltros() {
+    this.pessoasFiltradas = this.pessoas.filter((pessoa) => {
+      return (
+        pessoa.nome.toLowerCase().includes(this.filtro.nome.toLowerCase()) &&
+        (this.filtro.sexo === '' || pessoa.sexo === this.filtro.sexo) &&
+        (this.filtro.estadoCivil === '' || pessoa.estadoCivil === this.filtro.estadoCivil) &&
+        (this.filtro.data === '' || this.compararDatas(pessoa.dtNascimento, this.filtro.data))
+      );
+    });
+  }
+
+  compararDatas(dataPessoa: string, dataFiltro: string): boolean {
+    const dataPessoaObj = new Date(dataPessoa);
+    const dataFiltroObj = new Date(dataFiltro);
+    
+    dataPessoaObj.setHours(0, 0, 0, 0);
+    dataFiltroObj.setHours(0, 0, 0, 0);
+    
+    return dataPessoaObj.getTime() === dataFiltroObj.getTime();
   }
 }
